d7ac77216679e71659eab52bd46583decb665d84
HodaJahangiri <hoda.cs87@gmail.com>
2025-10-05 16:47:06 -0700

    Initial commit, question1 is solved.

---
+++ question1.py
@@ -0,0 +1,33 @@
+# a budget calculator
+
+def calculate_budget(monthly_income, rent_expense, food_expense, entertainment_expense):
+    total_expenses = rent_expense + food_expense + entertainment_expense
+    remaining_money = monthly_income - total_expenses
+    print("=== BUDGET SUMMARY ===")
+    print(f"Monthly Income: ${monthly_income:.2f}")
+    print(f"Total Expenses: ${total_expenses}")
+    print(f"Remaining Money: ${remaining_money}")
+    if remaining_money < 0:
+        print ("Budget Advice: You're overspending! Cut back on expenses.")
+    elif remaining_money < 100:
+        print ("Budget Advice: Your budget is tight. Be careful with spending.")
+    else:
+        print ("Budget Advice: Great job! You have money left over.")
+
+
+stop = False
+while stop == False:
+    try:
+        monthly_income = float(input("Enter your monthly income: "))
+        rent_expense = float(input("Enter rent expense: "))
+        food_expense = float(input("Enter food expense: "))
+        entertainment_expense = float(input("Enter entertainment expense: "))
+        calculate_budget(monthly_income, rent_expense, food_expense, entertainment_expense)
+    except ValueError:
+        print("Enter a valid price! ")
+        continue
+    stop_variable = input("To stop calculating press: No/N ")
+    if stop_variable.lower() in ["no", "n"]:
+        stop = True
+
+        
